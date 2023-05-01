56d765dd1cf6702e959aa7c77218de360839973e
unknown <nikhilchakravarthy1303@gmail.com>
2023-05-01 10:13:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div>
+      <a href="https://zegocloud.github.io/zego_uikit_prebuilt_web/video_conference/index.html" target="_blank" rel="noopener noreferrer">
+          <button>Zego</button>
+        </a>
+        <a href="https://videomeet-production-77ab.up.railway.app/action.html" target="_blank" rel="noopener noreferrer">
+          <button>Sockets</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
